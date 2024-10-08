74e3481917edfb1da89b4fcc3875129ed2db91cd
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2024-10-08 02:10:57 -0400

    add insertRecursion function
    The main purpose of this function is to insert a node in the binary search tree depending on the following conditions:
    - if the value is less than currentNode value, we start going deeper on the left subtree
    - if the value is greater than currentNode value, we start going deeper on the right subtree
    
    Initially this function was done iteratively but recursion was more suited for this function based on the conditions above.

--- binarySearch.js
+++ binarySearch.js
@@ -12,148 +12,76 @@ class Tree {
   }
 
   buildTree(array, start = 0, end = array.length - 1) {
-    
-    if(array.length === 0){
+    if (array.length === 0) {
       return null;
     }
 
-    if(start > end) return null;
+    if (start > end) return null;
 
-    let mid = Math.floor((start + end)/2);
+    let mid = Math.floor((start + end) / 2);
 
     let rootValue = new Node(array[mid]);
 
-
-
-    rootValue.left = this.buildTree(array, start, mid-1);
-    rootValue.right = this.buildTree(array, mid+1, end);
-
-
+    rootValue.left = this.buildTree(array, start, mid - 1);
+    rootValue.right = this.buildTree(array, mid + 1, end);
 
     return rootValue;
   }
 
-  insert(value){
-    // value is X, the node to be inserted
-    //we need to compare the root to X, if root < X, go left, else go right
-    let newNode = new Node(value);
+  insertRecursion(value, currentNode){
+    // base case: the obvious result is that if the the current node value is null, we assign that current node value the newNode
+    // Otherwise: if the value is less than the node.value, we'll keep going down the left subtree, else if the value is greater than the node.value, we'll keep going down the right subtree
 
-    if(this.root === null){
-      this.root = newNode;
-
-      return this.root;
+    if(currentNode === null){
+      // If the node 'this.value' is null, we can attach the new node, else we keep going down depending on the condition
+      return new Node(value);
     }
 
-    let currentNode = this.root;
-
-    while(currentNode){
-
-      if(value < currentNode.value){
-        if(currentNode.left === null){
-          currentNode.left = newNode;
-          break;
-        }else{
-          currentNode = currentNode.left;
-        }
-      }else if(value > currentNode.value){
-        if(currentNode.right === null){
-          currentNode.right = newNode;
-        }else{
-          currentNode = currentNode.right;
-        }
-      }else{
-        break;
-      }
-
-
-      
+    if(currentNode === value){
+      return currentNode;
     }
 
-    return this.root;
-    
-  }
-
-  deleteItem(value){
-    let currentNode = this.root;
-    let parentNode = null;
-
-
-    while(currentNode){
-      if(value < currentNode.value){
-        parentNode = currentNode;
-        currentNode = currentNode.left;
-      }else if(value > currentNode.value){
-        parentNode = currentNode;
-        currentNode = currentNode.right;
-      }else if(value === currentNode.value){
-        //base case 1
-        if(currentNode.left === null && currentNode.right === null){
-          if(parentNode.left.value === value){
-            parentNode.left = null;
-          }else if(parentNode.right.value === value){
-            parentNode.right = null;
-          }else{
-            return this.root;
-          }
-          
-        }
-        else if((currentNode.left === null && currentNode.right !== null) || (currentNode.right === null && currentNode.left !== null)){
-          if(currentNode.left !== null && currentNode.right === null){
-            console.log("_________________ LEFT NOT NULL");
-            parentNode.left = currentNode.left;
-            currentNode = parentNode.left;
-   
-
-          }else if(currentNode.right !== null && currentNode.left === null){
-            console.log("__________________ RIGHT NOT NULL");
-            parentNode.right = currentNode.right;
-            currentNode = parentNode.right;
-          }
-        }else{
-          return this.root;
-        }
-        break;
-      }
+    if(value < currentNode.value){
+      currentNode.left = this.insertRecursion(value, currentNode.left);
+    }
+    else if(value > currentNode.value){
+      currentNode.right = this.insertRecursion(value,currentNode.right);
     }
 
     return currentNode;
-
   }
-
 }
-//we need a sort method which include to remove duplicates of an array
 
-function merge_sort(array){
-  if(array.length === 1){
+function merge_sort(array) {
+  if (array.length === 1) {
     return array;
-  }else{
+  } else {
     let result = [];
-    let cutArrayInHalf = Math.floor(array.length/2);
+    let cutArrayInHalf = Math.floor(array.length / 2);
     let leftHalf = merge_sort(array.slice(0, cutArrayInHalf));
     let rightHalf = merge_sort(array.slice(cutArrayInHalf));
-    
+
     let i = 0;
     let j = 0;
     let k = 0;
 
-    while( i < leftHalf.length && j < rightHalf.length){
-      if(leftHalf[i] === rightHalf[j]){
-
+    while (i < leftHalf.length && j < rightHalf.length) {
+      if (leftHalf[i] === rightHalf[j]) {
       }
-      if(leftHalf[i] < rightHalf[j]){
+      if (leftHalf[i] < rightHalf[j]) {
         result[k++] = leftHalf[0];
         i++;
-      }else{
+      } else {
         result[k++] = rightHalf[j];
         j++;
       }
     }
 
-    for(; i < leftHalf.length; i++){
+    for (; i < leftHalf.length; i++) {
       result[k++] = leftHalf[i];
     }
 
-    for(; j < rightHalf.length; j++){
+    for (; j < rightHalf.length; j++) {
       result[k++] = rightHalf[j];
     }
 
@@ -161,42 +89,31 @@ function merge_sort(array){
   }
 }
 
-let not_sorted = [4,3,2,1,];
+// let not_sorted = [4, 3, 2, 1];
 
-let sorted = merge_sort(not_sorted);
+// let sorted = merge_sort(not_sorted);
 
 let test1 = new Tree();
 
-
-test1.insert(10);
-test1.insert(5);
-test1.insert(2);
-test1.insert(3);
-test1.insert(1);
-test1.insert(20);
+let returnNode = test1.buildTree([4]);
 
 
-
-function prettyPrint(node, prefix = "", isLeft = true){
+function prettyPrint(node, prefix = '', isLeft = true) {
   if (node === null) {
     return;
   }
   if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
   }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
   if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
   }
-};
+}
+
+prettyPrint(returnNode);
+
+test1.insertRecursion(6, returnNode);
 
 
-
-test1.insert(14);
-test1.insert(11);
-
-prettyPrint(test1.root);
-
-test1.deleteItem(14);
-
-prettyPrint(test1.root);
+prettyPrint(returnNode);
